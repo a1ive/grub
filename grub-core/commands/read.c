49418040390e5230572dbb5c650e8e6eee5bf5b8
a1ive <ligenlive@gmail.com>
2019-01-07 20:52:44 +0800

    Add read_file command

--- grub-core/commands/read.c
+++ grub-core/commands/read.c
@@ -20,6 +20,7 @@
 #include <grub/dl.h>
 #include <grub/misc.h>
 #include <grub/mm.h>
+#include <grub/normal.h>
 #include <grub/env.h>
 #include <grub/term.h>
 #include <grub/types.h>
@@ -41,16 +42,49 @@ grub_cmd_read (grub_command_t cmd __attribute__ ((unused)), int argc, char **arg
   return 0;
 }
 
+static grub_err_t
+grub_cmd_read_from_file (grub_command_t cmd __attribute__ ((unused)), int argc, char **args)
+{
+  char *line;
+  int i = 0;
+  grub_file_t file;
+
+  if (argc < 1)
+    return grub_error (GRUB_ERR_BAD_ARGUMENT, N_("file name expected"));
+  if (argc < 2)
+    return grub_error (GRUB_ERR_BAD_ARGUMENT, N_("variable name expected"));
+  file = grub_file_open (args[i++], GRUB_FILE_TYPE_CAT);
+  if (! file)
+    return grub_errno;
+  while ( i < argc )
+    {
+      line = grub_file_getline (file);
+      if ( !line )
+	break;
+      grub_env_set (args[i++], line);
+      grub_free (line);
+    }
+  grub_file_close (file);
+  if (i != argc)
+    return GRUB_ERR_OUT_OF_RANGE;
+  return 0;
+}
+
 static grub_command_t cmd;
+static grub_command_t cme;
 
 GRUB_MOD_INIT(read)
 {
   cmd = grub_register_command ("read", grub_cmd_read,
 			       N_("[ENVVAR]"),
 			       N_("Set variable with user input."));
+  cme = grub_register_command ("read_file", grub_cmd_read_from_file,
+			       N_("FILE ENVVAR [...]"),
+			       N_("Set variable(s) with line(s) from FILE."));
 }
 
 GRUB_MOD_FINI(read)
 {
   grub_unregister_command (cmd);
+  grub_unregister_command (cme);
 }
