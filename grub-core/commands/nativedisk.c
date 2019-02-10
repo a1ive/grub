fba7dfbfd4f0c8a25e5bf85f1c76a9d514890da2
a1ive <ligenlive@gmail.com>
2019-02-10 13:15:32 +0800

    disk/vhd.c: Support dynamic VHD

--- grub-core/commands/nativedisk.c
+++ grub-core/commands/nativedisk.c
@@ -99,6 +99,7 @@ get_uuid (const char *name, char **uuid, int getnative)
     case GRUB_DISK_DEVICE_LOOPBACK_ID:
     case GRUB_DISK_DEVICE_DISKFILTER_ID:
     case GRUB_DISK_DEVICE_CRYPTODISK_ID:
+    case GRUB_DISK_DEVICE_VHD_ID:
       break;
     }
   if (dev)
