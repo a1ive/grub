2f1499a16a5bc0ab1d854727667bc61f8dbceb79
a1ive <ligenlive@gmail.com>
2019-09-19 18:19:37 +0800

    Don't write messages to the screen Writing messages to the screen before the menus or boot splash happens so quickly it looks like something is wrong and isn't very appealing.

--- grub-core/kern/main.c
+++ grub-core/kern/main.c
@@ -296,8 +296,8 @@ grub_main (void)
   grub_boot_time ("After machine init.");
 
   /* Hello.  */
-  //grub_setcolorstate (GRUB_TERM_COLOR_HIGHLIGHT);
-  //grub_printf ("Welcome to GRUB!\n\n");
+  grub_setcolorstate (GRUB_TERM_COLOR_HIGHLIGHT);
+  grub_printf ("Welcome to GRUB!\n\n");
   grub_setcolorstate (GRUB_TERM_COLOR_STANDARD);
 
   grub_load_config ();
