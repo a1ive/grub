31bd20204f255b27314ebb30b9c7da4961b327b1
a1ive <ligenlive@gmail.com>
2019-11-20 11:15:56 +0800

    [WIP] ntboot for uefi

---
+++ grub-core/map/ntboot/ntboot.c
@@ -0,0 +1,170 @@
+ /*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2019  Free Software Foundation, Inc.
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <grub/dl.h>
+#include <grub/efi/api.h>
+#include <grub/efi/efi.h>
+#include <grub/device.h>
+#include <grub/err.h>
+#include <grub/env.h>
+#include <grub/extcmd.h>
+#include <grub/file.h>
+#include <grub/i18n.h>
+#include <grub/misc.h>
+#include <grub/mm.h>
+#include <grub/types.h>
+#include <grub/term.h>
+
+#include <maplib.h>
+#include <private.h>
+#include <efiapi.h>
+#include <wimboot.h>
+#include <vfat.h>
+
+#include "ntboot.h"
+
+GRUB_MOD_LICENSE ("GPLv3+");
+
+static const struct grub_arg_option options_ntboot[] = {
+  {"gui", 'g', 0, N_("Display graphical boot messages."), 0, 0},
+  {"pause", 'p', 0, N_("Show info and wait for keypress."), 0, 0},
+  {"vhd", 'v', 0, N_("Boot NT6+ VHD/VHDX."), 0, 0},
+  {"wim", 'w', 0, N_("Boot NT6+ WIM."), 0, 0},
+  {"efi", 'e', 0, N_("Specify the bootmgfw.efi file."), N_("FILE"), ARG_TYPE_FILE},
+  {"sdi", 's', 0, N_("Specify the boot.sdi file."), N_("FILE"), ARG_TYPE_FILE},
+  {0, 0, 0, 0, 0, 0}
+};
+
+enum options_ntboot
+{
+  NTBOOT_GUI,
+  NTBOOT_PAUSE,
+  NTBOOT_VHD,
+  NTBOOT_WIM,
+  NTBOOT_EFI,
+  NTBOOT_SDI,
+};
+
+static grub_err_t
+grub_cmd_ntboot (grub_extcmd_context_t ctxt,
+                  int argc, char *argv[])
+{
+  struct grub_arg_list *state = ctxt->state;
+  grub_file_t bootmgr = 0;
+  grub_file_t bootsdi = 0;
+  grub_file_t file = 0;
+  char *filename = NULL;
+  enum boot_type type;
+
+  if (argc != 1)
+  {
+    grub_error (GRUB_ERR_BAD_ARGUMENT, N_("filename expected"));
+    goto fail;
+  }
+  file = grub_file_open (argv[0], GRUB_FILE_TYPE_GET_SIZE);
+  if (!file)
+  {
+    grub_error (GRUB_ERR_FILE_READ_ERROR, N_("failed to open file"));
+    goto fail;
+  }
+  if (!file->device || !file->device->disk ||
+      file->device->disk->name[0] != 'h' || !file->device->disk->partition)
+  {
+    grub_error (GRUB_ERR_BAD_DEVICE,
+                "this command is available only for disk devices");
+    goto fail;
+  }
+  if (argv[0][0] == '(')
+  {
+    filename = grub_strchr (argv[0], '/');
+    if (!filename)
+      goto fail;
+  }
+  else if (argv[0][0] == '/')
+  {
+    filename = &argv[0][0];
+  }
+  else
+    goto fail;
+
+  if (filename[grub_strlen (filename) - 1] == 'm' ||
+      filename[grub_strlen (filename) - 1] == 'M') /* wim */
+    type = BOOT_WIM;
+  else
+    type = BOOT_VHD;
+  if (state[NTBOOT_WIM].set)
+    type = BOOT_WIM;
+  if (state[NTBOOT_VHD].set)
+    type = BOOT_VHD;
+
+  wimboot_cmd.rawbcd = FALSE;
+  if (state[NTBOOT_GUI].set)
+    wimboot_cmd.gui = TRUE;
+  if (state[NTBOOT_PAUSE].set)
+    wimboot_cmd.pause = TRUE;
+  if (state[NTBOOT_EFI].set)
+    bootmgr = grub_file_open (state[NTBOOT_EFI].arg,
+                              GRUB_FILE_TYPE_EFI_CHAINLOADED_IMAGE);
+  else
+    bootmgr = grub_file_open ("/boot/bootmgfw.efi",
+                              GRUB_FILE_TYPE_EFI_CHAINLOADED_IMAGE);
+  if (!bootmgr)
+  {
+    grub_error (GRUB_ERR_FILE_READ_ERROR, N_("failed to open bootmgfw.efi"));
+    goto fail;
+  }
+  if (state[NTBOOT_SDI].set)
+    bootsdi = grub_file_open (state[NTBOOT_SDI].arg,
+                              GRUB_FILE_TYPE_LOOPBACK);
+  else
+    bootsdi = grub_file_open ("/boot/boot.sdi",
+                              GRUB_FILE_TYPE_LOOPBACK);
+  if (!bootsdi)
+  {
+    grub_error (GRUB_ERR_FILE_READ_ERROR, N_("failed to open boot.sdi"));
+    goto fail;
+  }
+  add_file ("bootmgfw.efi", bootmgr, bootmgr->size, efi_read_file);
+  add_file ("boot.sdi", bootsdi, bootsdi->size, efi_read_file);
+  bcd_patch (type, filename, file->device->disk->name,
+             file->device->disk->partition->start,
+             file->device->disk->partition->partmap->name);
+  add_file ("bcd", bcd, bcd_len, mem_read_file);
+  grub_getkey ();
+  wimboot_install ();
+  wimboot_boot (bootmgfw);
+fail:
+  if (file)
+    grub_file_close (file);
+  return grub_errno;
+}
+
+static grub_extcmd_t cmd_ntboot;
+
+GRUB_MOD_INIT(ntboot)
+{
+  cmd_ntboot = grub_register_extcmd ("ntboot", grub_cmd_ntboot, 0,
+                    N_("[-v|-w] [--efi=FILE] [--sdi=FILE] FILE"),
+                    N_("Boot NT6+ VHD/VHDX/WIM"), options_ntboot);
+}
+
+GRUB_MOD_FINI(ntboot)
+{
+  grub_unregister_extcmd (cmd_ntboot);
+}
---
+++ grub-core/map/ntboot/patch.c
@@ -0,0 +1,215 @@
+ /*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2019  Free Software Foundation, Inc.
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <grub/disk.h>
+#include <grub/misc.h>
+#include <grub/mm.h>
+#include <grub/types.h>
+#include <grub/term.h>
+#include <grub/msdos_partition.h>
+
+#include "ntboot.h"
+#include <stdint.h>
+#include <string.h>
+
+static void
+print_hex (grub_size_t offset,
+           const char *prefix, grub_size_t len, int hex)
+{
+  grub_size_t i, j;
+  unsigned char *p;
+  p = bcd + offset;
+  grub_printf ("0x%04lx %s:\n", (unsigned long)offset, prefix);
+  for (i = 0, j = 0; i < len; i++, j++, p++)
+  {
+    if (j == 64)
+    {
+      grub_printf ("\n");
+      j = 0;
+    }
+    if (hex)
+      grub_printf (" %02x", *p);
+    else
+    {
+      if (*p > 0x19 && *p < 0x7f)
+        grub_printf ("%c", *p);
+      else
+        grub_printf (".");
+    }
+  }
+  grub_printf ("\n");
+}
+
+static grub_size_t
+replace_hex (const unsigned char *search, grub_size_t search_len,
+             const unsigned char *replace, grub_size_t replace_len, int count)
+{
+  grub_size_t offset, last = 0;
+  int cnt = 0;
+  for (offset = 0; offset + search_len < bcd_len; offset++)
+  {
+    if (grub_memcmp (bcd + offset, search, search_len) == 0)
+    {
+      last = offset;
+      grub_printf (" 0x%04x", (unsigned) offset);
+      cnt++;
+      grub_memcpy (bcd + offset, replace, replace_len);
+      if (count && cnt == count)
+        break;
+    }
+  }
+  if (cnt)
+    grub_printf ("\n");
+  return last;
+}
+
+static void
+bcd_patch_guid_offset (enum boot_type type, grub_size_t offset)
+{
+  unsigned char *p;
+  if (bcd_len < offset)
+  {
+    grub_printf ("bad BCD file.");
+    return ;
+  }
+  p = bcd + offset;
+  if (*p != 'd')
+  {
+    grub_printf ("bad BCD file.");
+    return ;
+  }
+  print_hex (offset - 0x10, "guid", 76, 0);
+  if (type == BOOT_WIM)
+    *p = 'a';
+  if (type == BOOT_VHD)
+    *p = 'b';
+  print_hex (offset - 0x10, "replace", 76, 0);
+}
+
+static void
+bcd_patch_guid (enum boot_type type)
+{
+  bcd_patch_guid_offset (type, 0x343c);
+  bcd_patch_guid_offset (type, 0x35dc);
+}
+
+static void
+bcd_patch_path_offset (const char *search, const char *path)
+{
+  grub_size_t len = 2 * (grub_strlen (path) + 1);
+  wchar_t *upath = NULL;
+  grub_size_t offset;
+  grub_size_t i;
+  char c;
+  upath = grub_zalloc (len);
+  if (!upath)
+  {
+    grub_printf ("out of memory.");
+    return ;
+  }
+  for (i = 0; i < grub_strlen (path); i++)
+  {
+    c = path[i];
+    if (c == '/')
+      c = '\\';
+    upath[i] = c;
+  }
+  offset = replace_hex ((const unsigned char *)search, grub_strlen (search),
+                        (unsigned char *)upath, len, 2);
+  print_hex (offset, "path", len, 0);
+  grub_free (upath);
+}
+
+static void
+bcd_patch_path (enum boot_type type, const char *path)
+{
+  const char *wim_path = "\\WIM_FILE_PATH";
+  const char *vhd_path = "\\VHD_FILE_PATH";
+  if (type == BOOT_WIM)
+    bcd_patch_path_offset (wim_path, path);
+  if (type == BOOT_VHD)
+    bcd_patch_path_offset (vhd_path, path);
+}
+
+static void
+bcd_patch_mbr_offset (grub_uint8_t *start, grub_uint8_t *sgn)
+{
+  const unsigned char default_sgn[] = { 0x53, 0xb7, 0x53, 0xb7 };
+  const unsigned char default_start[] = { 0x00, 0x7e, 0x00, 0x00 };
+  grub_size_t offset;
+  /* starting lba */
+  offset = replace_hex (default_start, 4, start, 8, 0);
+  print_hex (offset, "replace", 8, 1);
+  /* unique signature */
+  offset = replace_hex (default_sgn, 4, sgn, 4, 0);
+  print_hex (offset, "replace", 4, 1);
+}
+
+static void
+bcd_patch_mbr (const char *diskname, grub_disk_addr_t lba)
+{
+  grub_uint8_t start[8];
+  grub_disk_t disk = 0;
+  struct grub_msdos_partition_mbr *mbr = NULL;
+  disk = grub_disk_open (diskname);
+  if (!disk)
+  {
+    grub_printf ("failed to open %s\n", diskname);
+    goto fail;
+  }
+  mbr = grub_zalloc (GRUB_DISK_SECTOR_SIZE);
+  if (!mbr)
+  {
+    grub_printf ("out of memory");
+    goto fail;
+  }
+  if (grub_disk_read (disk, 0, 0, GRUB_DISK_SECTOR_SIZE, mbr))
+  {
+    if (!grub_errno)
+      grub_printf ("premature end of disk");
+    goto fail;
+  }
+  *(grub_uint64_t *)start = lba << GRUB_DISK_SECTOR_BITS;
+  /* write */
+  bcd_patch_mbr_offset (start, mbr->unique_signature);
+fail:
+  if (mbr)
+    grub_free (mbr);
+  if (disk)
+    grub_disk_close (disk);
+}
+
+void
+bcd_patch (enum boot_type type, /* vhd or wim */
+           const char *path,    /* file path '/boot/boot.wim' */
+           const char *diskname,/* disk name 'hd0' */
+           grub_disk_addr_t lba,/* starting_lba */
+           const char *partmap) /* partition table 'msdos' */
+{
+  if (partmap[0] == 'g')
+  {
+    grub_printf ("GPT: not supported.\n");
+    return;
+  }
+  bcd_patch_guid (type);
+  bcd_patch_path (type, path);
+  grub_getkey ();
+  bcd_patch_mbr (diskname, lba);
+  grub_getkey ();
+}
