6717ac0369fc584ad3997f16c2643b628dae581b
a1ive <ligenlive@gmail.com>
2019-12-20 21:03:39 +0800

    date: add checktime command

--- grub-core/commands/date.c
+++ grub-core/commands/date.c
@@ -21,6 +21,7 @@
 #include <grub/err.h>
 #include <grub/misc.h>
 #include <grub/datetime.h>
+#include <grub/command.h>
 #include <grub/extcmd.h>
 #include <grub/env.h>
 #include <grub/i18n.h>
@@ -153,18 +154,193 @@ fail:
   return grub_error (GRUB_ERR_BAD_ARGUMENT, "invalid datetime");
 }
 
-static grub_extcmd_t cmd;
+static int
+safe_parse_maxint (char **str_ptr, unsigned long long *myint_ptr)
+{
+  char *ptr = *str_ptr;
+  unsigned long long myint = 0;
+  unsigned long long mult = 10;
+  int found = 0;
+  int negative = 0;
+
+  /*
+   *  The decimal numbers can be positive or negative, ranging from
+   *  0x80000000(the minimal int) to 0x7fffffff(the maximal int).
+   *  The hex numbers are not checked.
+   */
+
+  if (*ptr == '-') /* check whether or not the negative sign exists */
+  {
+    ptr++;
+    negative = 1;
+  }
+
+  /*
+   *  Is this a hex number?
+   */
+  if (*ptr == '0' && grub_tolower (*(ptr + 1)) == 'x')
+  {
+    ptr += 2;
+    mult = 16;
+  }
+
+  while (1)
+  {
+    /* A bit tricky. This below makes use of the equivalence:
+       (A >= B && A <= C) <=> ((A - B) <= (C - B))
+       when C > B and A is unsigned.  */
+    unsigned int digit;
+
+    digit = grub_tolower (*ptr) - '0';
+    if (digit > 9)
+    {
+      digit -= 'a' - '0';
+      if (mult == 10 || digit > 5)
+        break;
+      digit += 10;
+    }
+
+    found = 1;
+    /* we do not check for hex or negative */
+    if (mult == 10 && ! negative)
+    /* 0xFFFFFFFFFFFFFFFF == 18446744073709551615ULL */
+    if (myint > 1844674407370955161ULL ||
+        (myint == 1844674407370955161ULL && digit > 5))
+    {
+      grub_error (GRUB_ERR_OUT_OF_RANGE, "number overflow.");
+      return 0;
+    }
+    myint *= mult;
+    myint += digit;
+    ptr++;
+  }
+
+  if (!found)
+  {
+    grub_error (GRUB_ERR_BAD_NUMBER, "ERR_NUMBER_PARSING.");
+    return 0;
+  }
+
+  *str_ptr = ptr;
+  *myint_ptr = negative ? -myint : myint;
+
+  return 1;
+}
+
+static void
+checktime_help (void)
+{
+  grub_printf ("Usage: \n");
+  grub_printf ("  checktime MINUTE  HOUR   DAY   MONTH  DAY\n");
+  grub_printf ("                         (month)       (week)\n");
+  grub_printf ("           \\*    any value\n");
+  grub_printf ("            ,    value list separator\n");
+  grub_printf ("            -    range of values\n");
+  grub_printf ("            /    step values\n");
+}
+
+static grub_err_t
+grub_cmd_checktime (grub_command_t ctxt __attribute__ ((unused)),
+               int argc, char **args)
+{
+  struct grub_datetime datetime;
+  unsigned int limit[5][2] = {{0, 59}, {0, 23}, {1, 31}, {1, 12}, {0, 7}};
+  unsigned int field[5];
+  unsigned int i;
+
+  if (grub_get_datetime (&datetime))
+    return grub_errno;
+  if (argc != 5)
+  {
+    checktime_help ();
+    return GRUB_ERR_NONE;
+  }
+
+  field[0] = datetime.minute;
+  field[1] = datetime.hour;
+  field[2] = datetime.day;
+  field[3] = datetime.month;
+  field[4] = grub_get_weekday (&datetime);
+
+  for (i = 0; i < 5; i++)
+  {
+    char *p;
+    int ok = 0;
+
+    p = args[i];
+    while (1)
+    {
+      unsigned long long m1, m2, m3;
+      unsigned int j;
+      if (*p == '*')
+      {
+        m1 = limit[i][0];
+        m2 = limit[i][1];
+        p++;
+      }
+      else
+      {
+        if (! safe_parse_maxint (&p, &m1))
+          return 0;
+        if (*p == '-')
+        {
+          p++;
+          if (! safe_parse_maxint (&p, &m2))
+            return 0;
+        }
+        else
+          m2 = m1;
+      }
+
+      if ((m1 < limit[i][0]) || (m2 > limit[i][1]) || (m1 > m2))
+        return 0;
+      if (*p == '/')
+      {
+        p++;
+        if (! safe_parse_maxint (&p, &m3))
+          return 0;
+      }
+      else
+        m3 = 1;
+      for (j = m1; j <= m2; j+= m3)
+      {
+        if (j == field[i])
+        {
+          ok = 1;
+          break;
+        }
+      }
+
+      if (ok)
+        break;
+      if (*p == ',')
+        p++;
+      else
+        break;
+    }
+    if (!ok)
+      break;
+  }
+  return (i == 5) ? GRUB_ERR_NONE
+    : grub_error (GRUB_ERR_TEST_FAILURE, N_("false"));
+}
+
+static grub_extcmd_t cmd_date;
+static grub_command_t cmd_checktime;
 
 GRUB_MOD_INIT(date)
 {
-  cmd =
-    grub_register_extcmd ("date", grub_cmd_date, 0,
-			   N_("[[year-]month-day] [hour:minute[:second]]"),
-			   N_("Display/set current datetime."),
-               options);
+  cmd_date = grub_register_extcmd ("date", grub_cmd_date, 0,
+                      N_("[[year-]month-day] [hour:minute[:second]]"),
+                      N_("Display/set current datetime."), options);
+
+  cmd_checktime = grub_register_command ("checktime", grub_cmd_checktime,
+                      N_("min hour dom month dow"),
+                      N_("Check current date and time."));
 }
 
 GRUB_MOD_FINI(date)
 {
-  grub_unregister_extcmd (cmd);
+  grub_unregister_extcmd (cmd_date);
+  grub_unregister_command (cmd_checktime);
 }
