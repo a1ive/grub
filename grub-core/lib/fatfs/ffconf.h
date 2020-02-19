cf2c68ec497279ccc99c3d3e41bbbb477302daea
a1ive <ligenlive@gmail.com>
2020-02-19 12:58:21 +0800

    fatfs: disable 64-bit LBA and EXFAT for 32-bit

--- grub-core/lib/fatfs/ffconf.h
+++ grub-core/lib/fatfs/ffconf.h
@@ -1,3 +1,4 @@
+#include <grub/types.h>
 /*---------------------------------------------------------------------------/
 /  FatFs Functional Configurations
 /---------------------------------------------------------------------------*/
@@ -199,8 +200,11 @@
 /  for variable sector size mode and disk_ioctl() function needs to implement
 /  GET_SECTOR_SIZE command. */
 
-
+#if GRUB_CPU_SIZEOF_LONG == 8
 #define FF_LBA64		1
+#else
+#define FF_LBA64		0
+#endif
 /* This option switches support for 64-bit LBA. (0:Disable or 1:Enable)
 /  To enable the 64-bit LBA, also exFAT needs to be enabled. (FF_FS_EXFAT == 1) */
 
@@ -227,8 +231,11 @@
 /  Instead of private sector buffer eliminated from the file object, common sector
 /  buffer in the filesystem object (FATFS) is used for the file data transfer. */
 
-
+#if GRUB_CPU_SIZEOF_LONG == 8
 #define FF_FS_EXFAT		1
+#else
+#define FF_FS_EXFAT		0
+#endif
 /* This option switches support for exFAT filesystem. (0:Disable or 1:Enable)
 /  To enable exFAT, also LFN needs to be enabled. (FF_USE_LFN >= 1)
 /  Note that enabling exFAT discards ANSI C (C89) compatibility. */
