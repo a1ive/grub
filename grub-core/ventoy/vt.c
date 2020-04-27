983551285318830d162dd76cc6df4b6dda9e6c27
a1ive <10670106+a1ive@users.noreply.github.com>
2020-04-27 16:20:50 +0800

    add ventoy compatible module

---
+++ grub-core/ventoy/vt.c
@@ -0,0 +1,108 @@
+/*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2020  Free Software Foundation, Inc.
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <grub/types.h>
+#include <grub/misc.h>
+#include <grub/mm.h>
+#include <grub/env.h>
+#include <grub/err.h>
+#include <grub/dl.h>
+#include <grub/extcmd.h>
+#include <grub/i18n.h>
+#include "vt_compatible.h"
+
+#ifdef GRUB_MACHINE_EFI
+#include <grub/efi/api.h>
+#include <grub/efi/efi.h>
+#endif
+
+GRUB_MOD_LICENSE ("GPLv3+");
+
+#ifdef GRUB_MACHINE_PCBIOS
+static int
+get_vt_data (ventoy_os_param_t *data)
+{
+  grub_packed_guid_t ventoy_guid = VENTOY_GUID;
+  grub_addr_t addr = VENTOY_BIOS_ADDR_L;
+  while (addr < VENTOY_BIOS_ADDR_U)
+  {
+    if (grub_memcmp (&ventoy_guid, (void *)addr, sizeof (grub_packed_guid_t)))
+    {
+      addr++;
+      continue;
+    }
+    grub_printf ("VentoyOsParam found.\n");
+    grub_memcpy (data, (void *)addr, sizeof (ventoy_os_param_t));
+    return 1;
+  }
+  return 0;
+}
+#elif defined (GRUB_MACHINE_EFI)
+static int
+get_vt_data (ventoy_os_param_t *data)
+{
+  grub_efi_guid_t ventoy_guid = VENTOY_GUID;
+  void *value = NULL;
+  grub_size_t datasize = 0;
+  value = grub_efi_get_variable("VentoyOsParam", &ventoy_guid, &datasize);
+  if (!value || !datasize || datasize != sizeof (ventoy_os_param_t))
+  {
+    if (value)
+      grub_free (value);
+    return 0;
+  }
+  grub_printf ("VentoyOsParam found.\n");
+  grub_memcpy (data, value, sizeof (ventoy_os_param_t));
+  grub_free (value);
+  return 1;
+}
+#else
+static int
+get_vt_data (ventoy_os_param_t *data __attribute__ ((unused)))
+{
+  return 0;
+}
+#endif
+
+static grub_err_t
+grub_cmd_ventoy (grub_extcmd_context_t ctxt __attribute__ ((unused)),
+                 int argc, char **args)
+{
+  ventoy_os_param_t os_param;
+  if (!get_vt_data (&os_param))
+    return grub_error (GRUB_ERR_FILE_NOT_FOUND, N_("Ventoy data not found"));
+
+  if (argc > 0)
+    grub_env_set (args[0], os_param.vtoy_img_path);
+  else
+    grub_printf ("%s\n", os_param.vtoy_img_path);
+  return GRUB_ERR_NONE;
+}
+
+static grub_extcmd_t cmd;
+
+GRUB_MOD_INIT(ventoy)
+{
+  cmd = grub_register_extcmd ("ventoy", grub_cmd_ventoy, 0, N_("[VAR]"),
+                              N_("Get Ventoy information."), 0);
+}
+
+GRUB_MOD_FINI(ventoy)
+{
+  grub_unregister_extcmd (cmd);
+}
---
+++ grub-core/ventoy/vt_compatible.h
@@ -0,0 +1,100 @@
+/*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2020  Free Software Foundation, Inc.
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/******************************************************************************
+ *
+ * Copyright (c) 2020, longpanda <admin@ventoy.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __VENTOY_COMPATIBLE_H__
+#define __VENTOY_COMPATIBLE_H__
+
+#include <grub/types.h>
+
+#define VENTOY_COMPATIBLE_STR      "VENTOY COMPATIBLE"
+#define VENTOY_COMPATIBLE_STR_LEN  17
+
+#define VENTOY_GUID \
+  { 0x77772020, 0x2e77, 0x6576, \
+    { 0x6e, 0x74, 0x6f, 0x79, 0x2e, 0x6e, 0x65, 0x74 } \
+  }
+
+#define VENTOY_BIOS_ADDR_L 0x80000
+#define VENTOY_BIOS_ADDR_U 0xA0000
+
+#pragma pack(1)
+
+typedef struct ventoy_os_param
+{
+  /* Signature for the information
+   * the hex value is 20207777772e76656e746f792e6e6574
+   */
+  grub_packed_guid_t guid;
+  /* This value, when added to all other 511 bytes,
+   * results in the value 00h (using 8-bit addition calculations).
+   */
+  grub_uint8_t chksum;                // checksum
+  /* GUID to uniquely identify the USB drive */
+  grub_uint8_t vtoy_disk_guid[16];
+  /* The USB drive size in bytes */
+  grub_uint64_t vtoy_disk_size;
+  /* The partition ID (begin with 1) which hold the iso file */
+  grub_uint16_t vtoy_disk_part_id;
+  /* The partition filesystem 0:exfat 1:ntfs other:reserved */
+  grub_uint16_t vtoy_disk_part_type;
+  /* The full iso file path under the partition, ( begin with '/' ) */
+  char vtoy_img_path[384];
+  /* The iso file size in bytes */
+  grub_uint64_t vtoy_img_size;
+  /*
+   * Ventoy will write a copy of ventoy_image_location data into runtime memory
+   * this is the physically address and length of that memory.
+   * Address 0 means no such data exist.
+   * Address will be aligned by 4KB.
+   */
+  grub_uint64_t vtoy_img_location_addr;
+  grub_uint32_t vtoy_img_location_len;
+  /*
+   * These 32 bytes are reserved by ventoy.
+   *
+   * vtoy_reserved[0]: vtoy_break_level
+   * vtoy_reserved[1]: vtoy_debug_level
+   *
+   */
+  grub_uint8_t vtoy_reserved[32];    // Internal use by ventoy
+  grub_uint8_t reserved[31];
+} ventoy_os_param_t;
+
+#pragma pack()
+
+#endif
+
