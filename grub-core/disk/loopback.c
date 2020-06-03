3ae1fdf5e853b00659d14b4b7e5924e63db589c1
a1ive <10670106+a1ive@users.noreply.github.com>
2020-06-03 19:44:53 +0800

    loopback: implement blocklist support

--- grub-core/disk/loopback.c
+++ grub-core/disk/loopback.c
@@ -31,7 +31,6 @@ struct grub_loopback
 {
   char *devname;
   grub_file_t file;
-  void *addr;
   struct grub_loopback *next;
   unsigned long id;
 };
@@ -47,6 +46,70 @@ static const struct grub_arg_option options[] =
     {"mem", 'm', 0, N_("Copy to RAM."), 0, 0},
     {0, 0, 0, 0, 0, 0}
   };
+
+static grub_file_t
+loop_file_open (const char *name, int mem)
+{
+  grub_file_t file = 0;
+  grub_size_t size = 0;
+  enum grub_file_type type = GRUB_FILE_TYPE_LOOPBACK;
+
+  file = grub_file_open (name, type);
+  if (!file)
+    return NULL;
+  size = grub_file_size (file);
+  if (file->fs && file->fs->fast_blocklist)
+    grub_blocklist_convert (file);
+  if (mem)
+  {
+    void *addr = NULL;
+    char newname[100];
+    addr = grub_malloc (size + GRUB_DISK_SECTOR_SIZE - 1);
+    if (!addr)
+    {
+      grub_printf ("out of memory\n");
+      grub_file_close (file);
+      return NULL;
+    }
+    grub_printf ("Loading %s ...\n", name);
+    grub_file_read (file, addr, size);
+    grub_file_close (file);
+    grub_memset ((grub_uint8_t *)addr + size, 0, GRUB_DISK_SECTOR_SIZE - 1);
+    grub_snprintf (newname, 100, "mem:%p:size:%lld", addr, (unsigned long long)size);
+    file = grub_file_open (newname, type);
+  }
+  return file;
+}
+
+static void
+loop_file_close (grub_file_t file)
+{
+  if (!file)
+    return;
+  if (grub_ismemfile (file->name))
+    grub_free (file->data);
+  grub_file_close (file);
+}
+
+static grub_err_t
+loop_file_write (grub_file_t file,
+                 const void *buf, grub_size_t len, grub_off_t offset)
+{
+  if (grub_ismemfile (file->name))
+  {
+    grub_memcpy((grub_uint8_t *)(file->data) + offset, buf, len);
+    return GRUB_ERR_NONE;
+  }
+  else if (file->fs && grub_strcmp (file->fs->name, "blocklist") == 0)
+  {
+    grub_file_seek (file, offset);
+    grub_blocklist_write (file, buf, len);
+    return GRUB_ERR_NONE;
+  }
+  else
+    return grub_error (GRUB_ERR_NOT_IMPLEMENTED_YET,
+                       "loopback write is not supported");
+}
 
 /* Delete the loopback device NAME.  */
 static grub_err_t
@@ -69,9 +132,7 @@ delete_loopback (const char *name)
   *prev = dev->next;
 
   grub_free (dev->devname);
-  grub_file_close (dev->file);
-  if (dev->addr)
-    grub_free (dev->addr);
+  loop_file_close (dev->file);
   grub_free (dev);
 
   return 0;
@@ -85,7 +146,6 @@ grub_cmd_loopback (grub_extcmd_context_t ctxt, int argc, char **args)
   grub_file_t file;
   struct grub_loopback *newdev;
   grub_err_t ret;
-  void *addr = NULL;
 
   if (argc < 1)
     return grub_error (GRUB_ERR_BAD_ARGUMENT, "device name required");
@@ -97,17 +157,9 @@ grub_cmd_loopback (grub_extcmd_context_t ctxt, int argc, char **args)
   if (argc < 2)
     return grub_error (GRUB_ERR_BAD_ARGUMENT, N_("filename expected"));
 
-  file = grub_file_open (args[1], GRUB_FILE_TYPE_LOOPBACK);
+  file = loop_file_open (args[1], state[1].set);
   if (! file)
     return grub_errno;
-  if (state[1].set)
-  {
-    addr = grub_malloc (file->size + GRUB_DISK_SECTOR_SIZE - 1);
-    if (! addr)
-      goto fail;
-    grub_memset ((grub_uint8_t *)addr + file->size, 0, GRUB_DISK_SECTOR_SIZE - 1);
-    grub_file_read (file, addr, file->size);
-  }
 
   /* First try to replace the old device.  */
   for (newdev = loopback_list; newdev; newdev = newdev->next)
@@ -116,11 +168,8 @@ grub_cmd_loopback (grub_extcmd_context_t ctxt, int argc, char **args)
 
   if (newdev)
   {
-    grub_file_close (newdev->file);
-    if (newdev->addr)
-      grub_free (newdev->addr);
+    loop_file_close (newdev->file);
     newdev->file = file;
-    newdev->addr = addr;
     return 0;
   }
 
@@ -132,14 +181,11 @@ grub_cmd_loopback (grub_extcmd_context_t ctxt, int argc, char **args)
   newdev->devname = grub_strdup (args[0]);
   if (! newdev->devname)
   {
-    if (newdev->addr)
-      grub_free (newdev->addr);
     grub_free (newdev);
     goto fail;
   }
 
   newdev->file = file;
-  newdev->addr = addr;
   newdev->id = last_id++;
 
   /* Add the new entry to the list.  */
@@ -154,7 +200,6 @@ fail:
   return ret;
 }
 
-
 static int
 grub_loopback_iterate (grub_disk_dev_iterate_hook_t hook, void *hook_data,
                        grub_disk_pull_t pull)
@@ -182,15 +227,6 @@ grub_loopback_open (const char *name, grub_disk_t disk)
   if (! dev)
     return grub_error (GRUB_ERR_UNKNOWN_DEVICE, "can't open device");
 
-  if (dev->addr)
-  {
-    disk->total_sectors = (dev->file->size + GRUB_DISK_SECTOR_SIZE - 1)
-                          >> GRUB_DISK_SECTOR_BITS;
-    disk->max_agglomerate = GRUB_DISK_MAX_MAX_AGGLOMERATE;
-    disk->id = dev->id;
-    disk->data = dev;
-    return 0;
-  }
   /* Use the filesize for the disk size, round up to a complete sector.  */
   if (dev->file->size != GRUB_FILE_SIZE_UNKNOWN)
     disk->total_sectors = (dev->file->size + GRUB_DISK_SECTOR_SIZE - 1)
@@ -214,13 +250,6 @@ grub_loopback_read (grub_disk_t disk, grub_disk_addr_t sector,
 {
   grub_file_t file = ((struct grub_loopback *) disk->data)->file;
   grub_off_t pos = (sector + size) << GRUB_DISK_SECTOR_BITS;
-  char *addr = ((struct grub_loopback *) disk->data)->addr;
-  if (addr)
-  {
-    grub_memcpy (buf, addr + (sector << GRUB_DISK_SECTOR_BITS),
-                 size << GRUB_DISK_SECTOR_BITS);
-    return 0;
-  }
 
   grub_file_seek (file, sector << GRUB_DISK_SECTOR_BITS);
   grub_file_read (file, buf, size << GRUB_DISK_SECTOR_BITS);
@@ -242,16 +271,10 @@ static grub_err_t
 grub_loopback_write (grub_disk_t disk, grub_disk_addr_t sector,
                      grub_size_t size, const char *buf)
 {
-  char *addr = ((struct grub_loopback *) disk->data)->addr;
-  if (addr)
-  {
-    grub_memcpy (addr + (sector << GRUB_DISK_SECTOR_BITS), buf,
-                 size << GRUB_DISK_SECTOR_BITS);
-    return 0;
-  }
+  grub_file_t file = ((struct grub_loopback *) disk->data)->file;
 
-  return grub_error (GRUB_ERR_NOT_IMPLEMENTED_YET,
-                     "loopback write is not supported");
+  return loop_file_write (file, buf, size << GRUB_DISK_SECTOR_BITS,
+                          sector << GRUB_DISK_SECTOR_BITS);
 }
 
 static struct grub_disk_dev grub_loopback_dev =
