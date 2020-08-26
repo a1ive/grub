25fc00daa90e0a79abee13b7f19726736163c881
a1ive <10670106+a1ive@users.noreply.github.com>
2020-08-26 17:19:33 +0800

    commands/expr: add hex and unsigned options

--- grub-core/commands/expr.c
+++ grub-core/commands/expr.c
@@ -1,6 +1,6 @@
 /*
  *  GRUB  --  GRand Unified Bootloader
- *  Copyright (C) 2019  Free Software Foundation, Inc.
+ *  Copyright (C) 2019,2020  Free Software Foundation, Inc.
  *
  *  GRUB is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -31,65 +31,23 @@ static const struct grub_arg_option options[] =
 {
   {"set", 's', 0, N_("Store the result in a variable."),
     N_("VARNAME"), ARG_TYPE_STRING},
+  {"unsigned", 'u', 0, N_("Calculate unsigned values."), 0, 0},
+  {"hex", 'x', 0, N_("Display the result in hexadecimal form."), 0, 0},
   {0, 0, 0, 0, 0, 0}
 };
 
-int64_t eval_expr_0 (char **ps);
-
-static int64_t parse_sgn (int32_t sign, int64_t num)
+enum options
 {
-  if (sign > 0)
-    return num;
-  else
-    return (0 - num);
-}
-
-static int64_t div64s (int64_t n, int64_t d)
-{
-  if (!d)
-  {
-    grub_printf ("ERROR: division by zero.\n");
-    return 1;
-  }
-  return grub_divmod64s (n, d, NULL);
-}
-
-static int64_t mod64s (int64_t n, int64_t d)
-{
-  int64_t r;
-  if (!d)
-  {
-    grub_printf ("ERROR: division by zero.\n");
-    return 1;
-  }
-  grub_divmod64s (n, d, &r);
-  return r;
-}
-
-static int64_t do_op (int64_t lhs, int64_t rhs, char op)
-{
-  if (op == '+')
-    return (lhs + rhs);
-  else if (op == '-')
-    return (lhs - rhs);
-  else if (op == '*')
-    return (lhs * rhs);
-  else if (op == '/')
-    return div64s (lhs, rhs);
-  else if (op == '%')
-    return mod64s (lhs, rhs);
-  else
-    return 0;
-}
+  EXPR_SET,
+  EXPR_U64,
+  EXPR_HEX,
+};
 
 static char *suppr_spaces (char *str)
 {
-  int64_t i;
-  int64_t j;
+  uint64_t i = 0, j = 0;
   char *str2 = NULL;
 
-  i = 0;
-  j = 0;
   str2 = malloc (sizeof (str) + 1);
   if (!str2)
     return NULL;
@@ -106,103 +64,45 @@ static char *suppr_spaces (char *str)
   return (str2);
 }
 
-static int64_t parse_nbr (char **ps)
-{
-  int64_t nbr;
-  int32_t sign;
+/* unsigned */
+#define EXPR_INT64 uint64_t
+#define SUFFIX(x) x
+#include "exprXX.c"
 
-  nbr = 0;
-  sign = 1;
-  if ((*ps)[0] == '+' || (*ps)[0] == '-')
-  {
-    if ((*ps)[0] == '-')
-      sign = -1;
-    *ps = *ps + 1;
-  }
-  if ((*ps)[0] == '(')
-  {
-    *ps = *ps + 1;
-    nbr = eval_expr_0 (ps);
-    if ((*ps)[0] == ')')
-      *ps = *ps + 1;
-    return parse_sgn (sign, nbr);
-  }
-  while ('0' <= (*ps)[0] && (*ps)[0] <= '9')
-  {
-    nbr = (nbr * 10) + (*ps)[0] - '0';
-    *ps = *ps + 1;
-  }
-  return parse_sgn (sign, nbr);
-}
-
-static int64_t eval_expr_1 (char **ps)
-{
-  int64_t lhs;
-  int64_t rhs;
-  char op;
-
-  lhs = parse_nbr (ps);
-  while ((*ps)[0] == '*' || (*ps)[0] == '/' || (*ps)[0] == '%')
-  {
-    op = (*ps)[0];
-    *ps = *ps + 1;
-    rhs = parse_nbr (ps);
-    lhs = do_op (lhs, rhs, op);
-  }
-  return (lhs);
-}
-
-int64_t eval_expr_0 (char **ps)
-{
-  int64_t lhs;
-  int64_t rhs;
-  char op;
-
-  lhs = parse_nbr(ps);
-  while ((*ps)[0] != '\0' && (*ps)[0] != ')')
-  {
-    op = (*ps)[0];
-    *ps = *ps + 1;
-    if (op == '+' || op == '-')
-      rhs = eval_expr_1 (ps);
-    else
-      rhs = parse_nbr (ps);
-    lhs = do_op (lhs, rhs, op);
-  }
-  return (lhs);
-}
-
-static int64_t eval_expr (char *str)
-{
-  int64_t ret;
-  char *str2 = NULL;
-  str2 = suppr_spaces (str);
-  if (!str2)
-    return 0;
-  str = str2;
-  ret = eval_expr_0 (&str);
-  grub_free (str2);
-  return ret;
-}
+/* signed */
+#undef EXPR_INT64
+#undef SUFFIX
+#define EXPR_INT64 int64_t
+#define SUFFIX(x) x ## s
+#include "exprXX.c"
 
 static grub_err_t
 grub_cmd_expr (grub_extcmd_context_t ctxt, int argc, char **args)
 {
   struct grub_arg_list *state = ctxt->state;
-  int64_t result;
+  uint64_t ret;
   char str[32];
 
   if (argc < 1)
-  return grub_error (GRUB_ERR_BAD_ARGUMENT, N_("expression expected"));
-  result = eval_expr (args[0]);
-
-  if (state[0].set)
+    return grub_error (GRUB_ERR_BAD_ARGUMENT, N_("expression expected"));
+  if (state[EXPR_U64].set)
   {
-    grub_snprintf (str, 32, "%lld", (long long)result);
-    grub_env_set (state[0].arg, str);
+    ret = eval_expr (args[0]);
+    grub_snprintf (str, 32, "%llu", (unsigned long long)ret);
   }
   else
-    grub_printf ("%lld\n", (long long)result);
+  {
+    ret = eval_exprs (args[0]);
+    grub_snprintf (str, 32, "%lld", (long long)ret);
+  }
+
+  if (state[EXPR_HEX].set)
+    grub_snprintf (str, 32, "0x%llx", (unsigned long long)ret);
+
+  if (state[EXPR_SET].set)
+    grub_env_set (state[EXPR_SET].arg, str);
+  else
+    grub_printf ("%s\n", str);
 
   return 0;
 }
