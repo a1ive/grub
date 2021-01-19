1f631b4f03f74ac92d09cc31191b8f0f812a5842
a1ive <10670106+a1ive@users.noreply.github.com>
2021-01-19 08:56:29 +0800

    Alignment correction for multiboot on 64bit

--- include/multiboot.h
+++ include/multiboot.h
@@ -206,7 +206,7 @@ struct multiboot_info
     {
       multiboot_uint32_t framebuffer_palette_addr;
       multiboot_uint16_t framebuffer_palette_num_colors;
-    };
+    } __attribute__((packed));
     struct
     {
       multiboot_uint8_t framebuffer_red_field_position;
@@ -216,7 +216,7 @@ struct multiboot_info
       multiboot_uint8_t framebuffer_blue_field_position;
       multiboot_uint8_t framebuffer_blue_mask_size;
     };
-  };
+  } __attribute__((packed));
 };
 typedef struct multiboot_info multiboot_info_t;
 
