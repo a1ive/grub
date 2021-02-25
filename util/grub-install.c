4b5f25a29557870adb84a1bf990e48fb7eb5852a
Daniel Kiper <daniel.kiper@oracle.com>
2021-02-25 18:35:01 +0100

    util/grub-install: Fix NULL pointer dereferences
    Two grub_device_open() calls does not have associated NULL checks
    for returned values. Fix that and appease the Coverity.
    
    Fixes: CID 314583
    
    Signed-off-by: Daniel Kiper <daniel.kiper@oracle.com>
    Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>

--- util/grub-install.c
+++ util/grub-install.c
@@ -1838,6 +1838,8 @@ main (int argc, char *argv[])
 	  fill_core_services (core_services);
 
 	  ins_dev = grub_device_open (install_drive);
+	  if (ins_dev == NULL)
+	    grub_util_error ("%s", grub_errmsg);
 
 	  bless (ins_dev, core_services, 0);
 
@@ -1938,6 +1940,8 @@ main (int argc, char *argv[])
 	  fill_core_services(core_services);
 
 	  ins_dev = grub_device_open (install_drive);
+	  if (ins_dev == NULL)
+	    grub_util_error ("%s", grub_errmsg);
 
 	  bless (ins_dev, boot_efi, 1);
 	  if (!removable && update_nvram)
