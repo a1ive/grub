1e159fae33641c43a6a232d8db92c028a494a67b
Miguel Ángel Arruga Vivas <rosen644835@gmail.com>
2021-04-03 15:33:33 +0200

    i18n: Format large integers before the translation message
    The GNU gettext only supports the ISO C99 macros for integral
    types. If there is a need to use unsupported formatting macros,
    e.g. PRIuGRUB_UINT64_T, according to [1] the number to a string
    conversion should be separated from the code printing message
    requiring the internationalization. So, the function grub_snprintf()
    is used to print the numeric values to an intermediate buffer and
    the internationalized message contains a string format directive.
    
    [1] https://www.gnu.org/software/gettext/manual/html_node/Preparing-Strings.html#No-string-concatenation
    
    Signed-off-by: Miguel Ángel Arruga Vivas <rosen644835@gmail.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

--- grub-core/kern/efi/mm.c
+++ grub-core/kern/efi/mm.c
@@ -157,7 +157,17 @@ grub_efi_allocate_pages_real (grub_efi_physical_address_t address,
 
   /* Limit the memory access to less than 4GB for 32-bit platforms.  */
   if (address > GRUB_EFI_MAX_USABLE_ADDRESS)
-    return NULL;
+    {
+      char inv_addr[17], max_addr[17]; /* log16(2^64) = 16, plus NUL. */
+
+      grub_snprintf (inv_addr, sizeof (inv_addr) - 1, "%" PRIxGRUB_UINT64_T,
+		     address);
+      grub_snprintf (max_addr, sizeof (max_addr) - 1, "%" PRIxGRUB_UINT64_T,
+		     (grub_efi_uint64_t) GRUB_EFI_MAX_USABLE_ADDRESS);
+      grub_error (GRUB_ERR_BAD_ARGUMENT,
+		  N_("invalid memory address (0x%s > 0x%s)"), inv_addr, max_addr);
+      return NULL;
+    }
 
   b = grub_efi_system_table->boot_services;
   status = efi_call_4 (b->allocate_pages, alloctype, memtype, pages, &address);
